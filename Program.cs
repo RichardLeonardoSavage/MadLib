8a007cebe74e6ea12e74d69f7730705398a591f0
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-07 01:39:48 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MadLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string color, pluralNoun, celebrity, breath;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.Write(" What are ogres like? ");
+            breath = Console.ReadLine();
+
+
+
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+            Console.WriteLine("My breath smells like " + breath); 
+
+            Console.ReadLine();
+        }
+    }
+}
